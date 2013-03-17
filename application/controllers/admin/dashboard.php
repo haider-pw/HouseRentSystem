79f579d3baad76b3410435a86dbea0f22704391a
pakistanihaider <pakistanihaider@gmail.com>
2013-03-17 13:07:47 +0500

    Added Applications Directory

---
+++ application/controllers/admin/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kill3rCoder-Lapi
+ * Date: 3/17/13
+ * Time: 12:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Dashboard extends Base_Controller{
+    
+}
---
+++ application/core/My_Controller.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kill3rCoder-Lapi
+ * Date: 3/17/13
+ * Time: 12:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class My_Controller extends CI_Controller{
+
+    public $data = array();
+
+    function __construct() {
+    parent::__construct();
+        $this->data['errors']=array();
+        $this->data['site_name']=array();
+} 
+}
