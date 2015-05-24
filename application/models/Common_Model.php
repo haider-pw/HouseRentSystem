44fe66128c47e1ec24d8c0448f51a98daf6e141e
pakistanihaider <pakistanihaider@gmail.com>
2015-05-24 13:04:35 +0500

    Some Good Changes Been Done..

--- application/models/Common_Model.php
+++ application/models/Common_Model.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Common_Model extends MY_Model{
+class common_model extends MY_Model{
 
     function __construct(){
         parent::__construct();
