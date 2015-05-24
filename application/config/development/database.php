e1ce2d844dcd8f0dc0825b652b50cdcc374f9244
pakistanihaider <pakistanihaider@gmail.com>
2015-05-24 13:29:46 +0500

    ignore some directory.

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'webteezy.com';
 $db['default']['username'] = 'webteezy';
-$db['default']['password'] = '3WJ~w*#d=xCv';
+$db['default']['password'] = '-------';
 $db['default']['database'] = 'houserentsystem';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
